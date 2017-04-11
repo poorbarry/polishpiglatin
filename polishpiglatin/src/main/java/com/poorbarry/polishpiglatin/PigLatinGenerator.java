0689f056c5ba24ef39366e9ea0fde77848b7c694
Dan <dan.muszynski@cengage.com>
2017-04-11 15:53:08 -0400

    Added basic handling of words starting with a vowel or a consonant.

--- polishpiglatin/src/main/java/com/poorbarry/polishpiglatin/PigLatinGenerator.java
+++ polishpiglatin/src/main/java/com/poorbarry/polishpiglatin/PigLatinGenerator.java
@@ -1,9 +1,32 @@
 package com.poorbarry.polishpiglatin;
 
+import java.util.regex.Pattern;
+
 public class PigLatinGenerator {
+
+	private static final String DASH = "-";
+	private static final String AY = "ay";
+	private static final String YAY = DASH + "yay";
+	private static final Pattern STARTS_WITH_VOWEL = Pattern.compile("^[aeiouy]", Pattern.CASE_INSENSITIVE);
 
 	public static void main(String[] args) {
 
+	}
+
+	public String translate(String text) {
+		if (startsWithVowel(text)) {
+			return text + YAY;
+		}
+		return convertWordStartingWithConsonant(text);
+	}
+
+	private String convertWordStartingWithConsonant(String text) {
+		char leadingConsonant = text.charAt(0);
+		return text.substring(1) + DASH + leadingConsonant + AY;
+	}
+
+	private boolean startsWithVowel(String text) {
+		return STARTS_WITH_VOWEL.matcher(text).find();
 	}
 
 }
--- polishpiglatin/src/test/java/com/poorbarry/polishpiglatin/PigLatinGeneratorTest.java
+++ polishpiglatin/src/test/java/com/poorbarry/polishpiglatin/PigLatinGeneratorTest.java
@@ -1,24 +1,45 @@
 package com.poorbarry.polishpiglatin;
 
-import org.junit.After;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PigLatinGeneratorTest {
 
+	private PigLatinGenerator generator;
+
 	@Before
 	public void setUp() throws Exception {
+		generator = new PigLatinGenerator();
 	}
 	
 	@Test
 	public void generatorExists() throws Exception {
-		PigLatinGenerator generator = new PigLatinGenerator();
 		Assert.assertTrue(generator != null);
 	}
-
-	@After
-	public void tearDown() throws Exception {
+	
+	@Test
+	public void startsWithVowel_AddsYay() throws Exception {
+		assertThat(generator.translate("a"), is("a-yay"));
+		assertThat(generator.translate("e"), is("e-yay"));
+		assertThat(generator.translate("i"), is("i-yay"));
+		assertThat(generator.translate("o"), is("o-yay"));
+		assertThat(generator.translate("u"), is("u-yay"));
+		assertThat(generator.translate("apple"), is("apple-yay"));
+	}
+	
+	@Test
+	public void startsWithConsonant_MovesConsonantToEndAndAddsAy() throws Exception {
+		assertThat(generator.translate("dog"), is("og-day"));
 	}
 
+	@Ignore
+	@Test
+	public void startsWithConsonantCluster_MovesConsonantClusterToEndAndAddsAy() throws Exception {
+		assertThat(generator.translate("frog"), is("og-fray"));
+	}
 }
